689d7d528ab6b933bbe8a5efb0f1593ae155c48e
Luciano Labudia <labudia.luciano@gmail.com>
2023-03-20 19:26:05 -0300

    Mostrar Textos de Forma condicional en React

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -9,18 +9,32 @@ export const PatientList = ({ patient }) => {
     return (
         <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
 
-            <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
-            <p className='text-xl mt-5 mb-10 text-center'>
-                Administra tus {''}
-                <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
-            </p>
+            { patient && patient.length ? (
+                <>
+                    <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+                    <p className='text-xl mt-5 mb-10 text-center'>
+                        Administra tus {''}
+                        <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+                    </p>
+        
+                    { patient.map( patient => (
+                        <Patient
+                            key={ patient.id }
+                            patient={ patient }
+                        />
+                    )) }
 
-            { patient.map( patient => (
-                <Patient
-                    key={ patient.id }
-                    patient={ patient }
-                />
-            )) }
+                </>
+
+            ) : (
+                <>
+                    <h2 className='font-black text-3xl text-center'>No hay Pacientes</h2>
+                    <p className='text-xl mt-5 mb-10 text-center'>
+                        Comienza agregando pacientes {''}
+                        <span className='text-indigo-600 font-bold'>y apareceran en este lugar</span>
+                    </p>
+                </>
+            ) }            
 
         </div>
     )
